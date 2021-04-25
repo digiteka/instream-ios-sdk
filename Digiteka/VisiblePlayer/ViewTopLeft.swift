11a8a5f75345a80c91bb369c1f16166dc49903a5
ceomat <crandrianambinina@ingenosya.mg>
2021-04-25 08:26:48 +0300

    avant merge Rilah

---
+++ Digiteka/VisiblePlayer/ViewTopLeft.swift
@@ -0,0 +1,30 @@
+//
+//  ViewTopLeft.swift
+//  Digiteka
+//
+//  Created by INGENOSYA SA on 21/04/2021.
+//  Copyright © 2021 INGENOSYA SA. All rights reserved.
+//
+
+import UIKit
+
+class ViewTopLeft: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Digiteka/nib/TopLeftV.swift
@@ -0,0 +1,21 @@
+//
+//  TopLeftV.swift
+//  Digiteka
+//
+//  Created by INGENOSYA SA on 23/04/2021.
+//  Copyright © 2021 INGENOSYA SA. All rights reserved.
+//
+
+import UIKit
+
+class TopLeftV: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
