b2b987abd54d534fd4eb5a0bfc11bbdc706f0d4b
Tsiaro <tandrianotahina@ingenosya.mg>
2021-10-19 11:40:35 +0300

    update SDK without scroll

---
+++ builds/Digiteka.xcframework/ios-arm64_x86_64-simulator/Digiteka.framework/Headers/Digiteka.h
@@ -0,0 +1,19 @@
+//
+//  Digiteka.h
+//  Digiteka
+//
+//  Created by INGENOSYA SA on 13/04/2021.
+//  Copyright © 2021 INGENOSYA SA. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Digiteka.
+FOUNDATION_EXPORT double DigitekaVersionNumber;
+
+//! Project version string for Digiteka.
+FOUNDATION_EXPORT const unsigned char DigitekaVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Digiteka/PublicHeader.h>
+
+
